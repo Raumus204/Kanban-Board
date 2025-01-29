4db12601a744e1ef7e8a3d3757ae15da05b4224c
Raumus204 <enacra204@gmail.com>
2025-01-29 12:12:06 -0600

    first commit

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,62 @@
+import { useState, FormEvent, ChangeEvent } from "react";
+
+import Auth from '../utils/auth';
+import { login } from "../api/authAPI";
+
+const Login = () => {
+  const [loginData, setLoginData] = useState({
+    username: '',
+    password: ''
+  });
+
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    setLoginData({
+      ...loginData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    setErrorMessage(null);
+
+    try {
+      const data = await login(loginData);
+      Auth.login(data.token);
+    } catch (err) {
+      console.error('Failed to login', err);
+      setErrorMessage('Failed to Login');
+    }
+  };
+
+  return (
+    <div className='container'>
+      <form className='form' onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        <label >Username</label>
+        <input 
+          type='text'
+          name='username'
+          value={loginData.username || ''}
+          onChange={handleChange}
+        />
+      <label>Password</label>
+        <input 
+          type='password'
+          name='password'
+          value={loginData.password || ''}
+          onChange={handleChange}
+        />
+        <button type='submit'>Submit Form</button>
+        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+      </form>
+      
+    </div>
+    
+  )
+};
+
+export default Login;
